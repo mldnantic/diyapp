1c379665bac89d303f80c0835e237b23cf708307
mldnantic <m.antic@elfak.rs>
2026-02-01 18:22:38 +0100

    feat(auth): Add authentication using Passport.js and JWT

---
+++ diyapp-backend/src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/models/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) { }
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.getByUsername(username);
+    if (user && await bcrypt.compare(pass, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+    async login(user: User) {
+    const payload = { username: user.username, sub: user.id, email: user.email, role: user.role };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+  
+}
