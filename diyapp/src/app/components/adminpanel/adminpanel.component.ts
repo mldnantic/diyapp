113147ae6a5253272be4cb422112b9a6e5611e2f
mldnantic <m.antic@elfak.rs>
2026-02-02 18:02:38 +0100

    feat(adminpanel): Add table mockup to category options

--- diyapp/src/app/components/adminpanel/adminpanel.component.ts
+++ diyapp/src/app/components/adminpanel/adminpanel.component.ts
@@ -5,15 +5,31 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
-import { RouterLink, RouterOutlet } from '@angular/router';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Wood'},
+  {position: 2, name: 'Tools'},
+  {position: 3, name: 'Metal'},
+  {position: 4, name: 'Decoration'}
+];
 
 @Component({
   selector: 'app-adminpanel.component',
-  imports: [RouterOutlet, RouterLink, MatButtonModule, MatTabsModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule, FormsModule],
+  imports: [MatButtonModule, MatTabsModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule, FormsModule, MatTableModule],
   templateUrl: './adminpanel.component.html',
   styleUrl: './adminpanel.component.scss',
 })
 export class AdminPanelComponent {
 
+  displayedColumns: string[] = ['position', 'name'];
+  dataSource = ELEMENT_DATA;
+
 }
+ 
