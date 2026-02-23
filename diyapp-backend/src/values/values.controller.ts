9c0d8bb7b0c0c12fcf7ae524e77e3c75bbe60a88
mldnantic <m.antic@elfak.rs>
2026-02-23 18:53:53 +0100

    feat(values): Add controller methods

--- diyapp-backend/src/values/values.controller.ts
+++ diyapp-backend/src/values/values.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import { Controller, Post, Get, Param, Body, Put, Delete } from '@nestjs/common';
 import { ValuesService } from './values.service';
-import { Value } from './models/value.entity';
 import { ValueDto } from './models/value.dto';
 
 @Controller('values')
 export class ValuesController {
-  constructor(private readonly valuesService: ValuesService) { }
+  constructor(private readonly valuesService: ValuesService) {}
 
   @Post()
   async addValue(@Body() dto: ValueDto) {
     return this.valuesService.addValue(dto);
   }
 
+  @Get('item/:itemId')
+  async getValuesOfItem(@Param('itemId') itemId: number) {
+    return this.valuesService.getValuesOfItem(itemId);
+  }
+
+  @Put(':id')
+  async updateValue(@Param('id') id: number, @Body() dto: ValueDto) {
+    return this.valuesService.updateValue(id, dto);
+  }
+
+  @Delete(':id')
+  async deleteValue(@Param('id') id: number) {
+    return this.valuesService.deleteValue(id);
+  }
 }
