425ae054eacb3fb7cd4f4a41c1e22338898f1192
mldnantic <m.antic@elfak.rs>
2026-03-10 09:36:41 +0100

    feat(app): Add backend guards

--- diyapp-backend/src/comments/comments.controller.ts
+++ diyapp-backend/src/comments/comments.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentDto } from './models/comment.dto';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles.decorator';
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) { }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator', 'user')
   @Post()
   async create(@Body() dto: CommentDto) {
     return this.commentsService.create(dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator')
   @Get('reported')
   async getReportedComments() {
     return this.commentsService.getReportedComments();
@@ -21,16 +28,22 @@ export class CommentsController {
     return this.commentsService.getCommentsForItem(itemId);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator', 'user')
   @Put(':id')
   async reportComment(@Param('id', ParseIntPipe) id: number) {
     return this.commentsService.reportComment(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator')
   @Put('/unreport/:id')
   async unreportComment(@Param('id', ParseIntPipe) id: number) {
     return this.commentsService.unreportComment(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator')
   @Delete(':id')
   async delete(@Param('id') id: number) {
     return this.commentsService.delete(id);
--- diyapp-backend/src/projects/projects.controller.ts
+++ diyapp-backend/src/projects/projects.controller.ts
@@ -1,41 +1,58 @@
-import { Controller, Post, Get, Param, Body, Put, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Post, Get, Param, Body, Put, Delete, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectDto, ProjectItemDto } from './models/project.dto';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles.decorator';
 
 @Controller('projects')
 export class ProjectsController {
-  constructor(private readonly projectService: ProjectsService) {}
+  constructor(private readonly projectService: ProjectsService) { }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator', 'user')
   @Post()
   createProject(@Body() dto: ProjectDto) {
     return this.projectService.createProject(dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator', 'user')
   @Get('/user/:id')
   findProjectsOfUser(@Param('id', ParseIntPipe) userId: number) {
     return this.projectService.findProjectsOfUser(userId);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator', 'user')
   @Get(':id')
   getProject(@Param('id', ParseIntPipe) id: number) {
     return this.projectService.getProject(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator', 'user')
   @Get('/items/:id')
   getItemsOfProject(@Param('id', ParseIntPipe) id: number) {
     return this.projectService.getItemsOfProject(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator', 'user')
   @Delete(':id')
   deleteProject(@Param('id', ParseIntPipe) id: number) {
     return this.projectService.deleteProject(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator', 'user')
   @Post(':id/items')
   addItemToProject(@Param('id', ParseIntPipe) id: number, @Body() dto: ProjectItemDto) {
     return this.projectService.addItemToProject(id, dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin', 'moderator', 'user')
   @Delete(':id/items/:itemId')
   removeItemFromProject(@Param('id', ParseIntPipe) id: number, @Param('itemId', ParseIntPipe) itemId: number) {
     return this.projectService.removeItemFromProject(id, itemId);
